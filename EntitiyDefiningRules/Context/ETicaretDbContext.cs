b388f90fdc3f4e390cc5200640425f01e954a126
farukakpinar52 <faruk.akpinar@hotmail.com>
2024-02-22 18:46:39 +0300

    Add project files.

---
+++ EntitiyDefiningRules/Context/ETicaretDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EntitiyDefiningRules.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyDefiningRules.Context
+{
+    public class ETicaretDbContext : DbContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Kullanım amaçları :
+            //hangi tür veritabanına karşılık davranış sergilenecek : Provider
+            //Hangi sunucuda olacak : ConnectionString
+            //Lazy & Eager loading hangisi olacak?
+            optionsBuilder.UseSqlServer("\"Server=DESKTOP-P7KA77K\\\\SQLEXPRESS;Database=CodefirstDB;User Id=sa;Password=1234; ;TrustServerCertificate=true\"");
+
+        }
+
+    }
+}
---
+++ EntitiyDefiningRules/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyDefiningRules.Entities
+{
+    public class Product
+    {
+        //EFCore her tabloda primary key'e ait bir  kolon olmamasını şart koşar. Primary Key'e karşılık gelen bu property tanımlı olmazsa hata verir.
+        //Bir entity içindeki propertytilerden adı ' Id , ID, UrunId, UrunID ' olan property EFCore tarafından PrimaryKey olarak kabul edilir.
+        public int Id { get; set; }
+    }
+}
